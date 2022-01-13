c982a57482ce448cf5551f308b0ac82f564072c0
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-01-13 12:34:41 -0800

    Time: 120 ms (44.03%), Space: 41.9 MB (7.16%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,17 +5,33 @@
  */
 
 // working inefficient solution:
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = 0; j < nums.length; j++) {
+//             if (i !== j) {
+//                 let test = nums[i] + nums[j];
+//                 if (test === target) {
+//                     return [i, j];
+//                 }
+//             } else {
+//                 continue;
+//             }
+//         }
+//     }
+// };
+// time complexity: quadratic, space complexity: constant
+
+//efficient solution:
+var twoSum = function (nums, target) {
+    let obj = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j) {
-                let test = nums[i] + nums[j];
-                if (test === target) {
-                    return [i, j];
-                }
-            } else {
-                continue;
-            }
+        obj[nums[i]] = i;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        let comp = target - nums[i];
+        if (`${comp}` in obj && obj[comp] !== i) {
+            return [i, obj[comp]];
         }
     }
 };
+//time complexity: linear, space complexity: linear
