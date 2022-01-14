9c4dbeeccf6f79ca091b235423460f90ed661f88
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-01-14 15:20:15 -0800

    Time: 379 ms (11.82%), Space: 49 MB (16.66%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let xStr = x.toString();
+    let test = xStr.split("").reverse().join("");
+    if (test === xStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
