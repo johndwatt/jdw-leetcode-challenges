e780303a705d570babc9af49a0dc7ce4d02ce174
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-01-20 15:22:59 -0800

    Time: 142 ms (5.23%), Space: 38.4 MB (87.41%) - LeetHub

--- 796-rotate-string/796-rotate-string.js
+++ 796-rotate-string/796-rotate-string.js
@@ -3,12 +3,22 @@
  * @param {string} goal
  * @return {boolean}
  */
+
+//inefficient
+// var rotateString = function(s, goal) {
+//     let splitStr = s.split("");
+//     for (let i = 0; i < s.length; i++){
+//         if (splitStr.join("") === goal) return true;
+//         splitStr.push(s[i]);
+//         splitStr.shift();
+//     }
+//     return false;
+// };
+
+//efficient 
 var rotateString = function(s, goal) {
-    let splitStr = s.split("");
-    for (let i = 0; i < s.length; i++){
-        if (splitStr.join("") === goal) return true;
-        splitStr.push(s[i]);
-        splitStr.shift();
-    }
+    if (s.length !== goal.length) return false;
+    let str = s+s;
+    if (str.includes(goal)) return true;
     return false;
 };
