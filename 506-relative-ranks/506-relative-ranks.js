4167f222834cc0e75188e1f5dfaa6eace256bbd5
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-01-21 14:04:44 -0800

    Time: 92 ms (85.27%), Space: 41.7 MB (46.88%) - LeetHub

--- 506-relative-ranks/506-relative-ranks.js
+++ 506-relative-ranks/506-relative-ranks.js
@@ -5,13 +5,13 @@
 var findRelativeRanks = function(score) {
     let answer = [];
     let saved = [...score];
-    let sorted = score.sort((a,b) => b - a);
+    score.sort((a,b) => b - a);
     let hash = {};
-    hash[sorted[0]] = "Gold Medal";
-    hash[sorted[1]] = "Silver Medal";
-    hash[sorted[2]] = "Bronze Medal";
-    for (let i = 3; i < sorted.length; i++){
-        hash[sorted[i]] = `${i + 1}`;
+    hash[score[0]] = "Gold Medal";
+    hash[score[1]] = "Silver Medal";
+    hash[score[2]] = "Bronze Medal";
+    for (let i = 3; i < score.length; i++){
+        hash[score[i]] = `${i + 1}`;
     }
     for (let i = 0; i < saved.length; i++){
         answer.push(hash[saved[i]]);
