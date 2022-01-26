90106d5f3a6da586e8a1e124acc84cdc63266a93
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-01-25 17:26:05 -0800

    Time: 88 ms (57.92%), Space: 41.7 MB (27.64%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.js
+++ 485-max-consecutive-ones/485-max-consecutive-ones.js
@@ -2,18 +2,33 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var findMaxConsecutiveOnes = function(nums) {
+//     let arr = [];
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] === 1){
+//             count++
+//         } else {
+//             arr.push(count);
+//             count = 0;
+//         }
+//         arr.push(count);
+//     }
+//     arr.sort((a,b) => b - a);
+//     return arr[0];
+// };
+
+//more efficient
 var findMaxConsecutiveOnes = function(nums) {
-    let arr = [];
+    let max = 0;
     let count = 0;
     for (let i = 0; i < nums.length; i++){
         if (nums[i] === 1){
-            count++
+            count++ 
+            if (max < count) max = count;
         } else {
-            arr.push(count);
             count = 0;
         }
-        arr.push(count);
     }
-    arr.sort((a,b) => b - a);
-    return arr[0];
+    return max;
 };
