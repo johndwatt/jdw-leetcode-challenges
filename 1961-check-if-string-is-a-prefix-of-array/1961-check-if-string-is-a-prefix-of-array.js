533490118e0f4b1d9b3442af5e6bc88808a8a366
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-02-12 14:47:19 -0800

    Time: 65 ms (95.28%), Space: 42.4 MB (14.96%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var isPrefixString = function(s, words) {
+    if (s.length < words[0].length) return false;
+    let word = 0;
+    let count = 0;
+    for (let i = 0; i < s.length; i++){
+        if (words[word] === undefined) return false;
+        if (s[i] !== words[word][count]) return false;
+        count++
+        if (words[word].length === count) {
+            word++;
+            count = 0;
+        }
+    }
+    return true;
+};
+
+var isPrefixString = function(s, words) {
+    if (s.length < words[0].length) return false;
+    let word = 0;
+    let count = 0;
+    for (let i = 0; i < s.length; i++){
+        if (words[word] === undefined) return false;
+        if (s[i] !== words[word][count]) return false;
+        count++;
+        if (words[word].length === count) {
+            word++;
+            count = 0;
+        }
+    }
+    if (count !== 0) return false;
+    return true;
+};
