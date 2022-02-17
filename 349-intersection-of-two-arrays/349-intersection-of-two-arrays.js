224e1ab33dc78ff12a4c0b34866364d7465aff94
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-02-16 19:40:15 -0800

    Time: 120 ms (22.22%), Space: 43.7 MB (20.73%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -18,7 +18,7 @@
 //     return result;
 // };
 
-// better time complexity
+// more efficient
 var intersection = function(nums1, nums2) {
     const compareSets = function(smallSet, largeSet){
         let result = [];
