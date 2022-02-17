3d28ba377db7d8043c562cf7cf384e211ab3a360
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-02-16 19:39:15 -0800

    Time: 72 ms (85.88%), Space: 43.7 MB (20.73%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -3,15 +3,36 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+// naive approach
+// var intersection = function(nums1, nums2) {
+//     let result = [];
+//     for (let i = 0; i < nums1.length; i++){
+//         if (result.includes(nums1[i])) continue;
+//         if (nums2.includes(nums1[i])){
+//             result.push(nums1[i]);
+//         } else {
+//             continue;
+//         }
+//     }
+//     return result;
+// };
+
+// 
 var intersection = function(nums1, nums2) {
-    let result = [];
-    for (let i = 0; i < nums1.length; i++){
-        if (result.includes(nums1[i])) continue;
-        if (nums2.includes(nums1[i])){
-            result.push(nums1[i]);
-        } else {
-            continue;
+    const compareSets = function(smallSet, largeSet){
+        let result = [];
+        for (let i = 0; i < smallSet.length; i++){
+            if (largeSet.includes(smallSet[i])) result.push(smallSet[i]);
         }
+        return result;
     }
-    return result;
+    let set1 = [...new Set(nums1)]
+    let set2 = [...new Set(nums2)]
+    
+    if (set1.length > set2.length){
+        return compareSets(set2, set1);
+    } else {
+        return compareSets(set1, set2);
+    }
 };
