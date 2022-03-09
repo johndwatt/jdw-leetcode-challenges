d69a28b9fa2f9e8569f9388e0475c2d1d1887546
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-03-08 18:06:30 -0800

    Time: 165 ms (7.37%), Space: 42.8 MB (42.11%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.js
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.js
@@ -2,19 +2,38 @@
  * @param {string} s
  * @return {string}
  */
+
+// inefficient
+// var modifyString = function(s) {
+//     if (s.includes('?') === false) return s;
+//     const letters = "abcdefghijklmnopqrstuvwxyz";
+//     for (let i = 0; i < s.length; i++){
+//         let counter = 0;
+//         if (s[i] === "?") {
+//             if (s[i+1] === letters[counter] || s[i-1] === letters[counter]) {
+//                 while (s[i+1] === letters[counter] || s[i-1] === letters[counter]) {
+//                     counter++;
+//                 }
+//                 s = s.substring(0, i) + letters[counter] + s.substring(i+1);
+//             } else {
+//                 s = s.substring(0, i) + letters[counter] + s.substring(i+1);
+//             }
+//         }
+//     }
+//     return s;
+// };
+
+// more efficient
 var modifyString = function(s) {
     if (s.includes('?') === false) return s;
-    const letters = "abcdefghijklmnopqrstuvwxyz";
     for (let i = 0; i < s.length; i++){
-        let counter = 0;
         if (s[i] === "?") {
-            if (s[i+1] === letters[counter] || s[i-1] === letters[counter]) {
-                while (s[i+1] === letters[counter] || s[i-1] === letters[counter]) {
-                    counter++;
-                }
-                s = s.substring(0, i) + letters[counter] + s.substring(i+1);
+            if (s[i+1] !== "a" && s[i-1] !== "a") {
+                s = s.slice(0, i) + "a" + s.slice(i+1);
+            } else if (s[i+1] !== "b" && s[i-1] !== "b"){
+                s = s.slice(0, i) + "b" + s.slice(i+1);
             } else {
-                s = s.substring(0, i) + letters[counter] + s.substring(i+1);
+                s = s.slice(0, i) + "c" + s.slice(i+1);
             }
         }
     }
