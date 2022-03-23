3a5ccba8d06db6a01ab2a67253a5f195287b9da2
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-03-23 14:45:37 -0700

    Time: 177 ms (42.34%), Space: 15.6 MB (80.76%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in nums:
-            if nums.count(i) == 1:
-                return i
+        # naive solution
+        # for i in nums:
+        #     if nums.count(i) == 1:
+        #         return i
+        
+        # better solution: XOR operator
+        result = 0
+        for num in nums:
+            result = result ^ num
+        return result
+        
