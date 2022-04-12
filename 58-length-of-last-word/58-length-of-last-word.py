87f341df752c51df2cb51309222391c1018f8a17
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-04-12 16:32:13 -0700

    Time: 24 ms (98.62%), Space: 13.8 MB (83.32%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,4 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        # Python split method w/o args will remove all whitespace
         split = s.split()
         return len(split[-1])
