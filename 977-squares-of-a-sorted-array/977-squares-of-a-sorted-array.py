e65db978833a126e448d2970fe499bf17bd155e2
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-04-12 17:14:39 -0700

    Time: 200 ms (99.58%), Space: 16.4 MB (19.20%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -5,3 +5,5 @@ class Solution:
             square = i * i
             result.append(square)
         return sorted(result)
+    
+    # sorted() returns a new list from the original list while sort() modifies the existing list
