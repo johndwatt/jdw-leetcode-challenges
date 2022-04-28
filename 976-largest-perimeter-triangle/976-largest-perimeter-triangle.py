e7b3d8037ca6acbfd409810446441b2e5aa7cd33
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-04-27 18:23:25 -0700

    Time: 370 ms (10.53%), Space: 15.4 MB (93.37%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -2,10 +2,10 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort()
         for i in range(len(nums) - 1, 1, -1):
-        # if the sum of 2 sides is larger than the last side
+        # if the sum of 2 sides is larger than the last side...
             if(nums[i-2] + nums[i-1] > nums[i]):
-                # return the area of the triangle
+                # return the area of the triangle...
                 return nums[i] + nums[i-1] + nums[i-2]
         return 0
             
-# A Non-zero triangle means the sum of 2 sides is larger than the last side
+# A non-zero triangle means the sum of 2 sides is larger than the last side
