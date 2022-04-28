7248e6f6f82321db8cd61188f1818fdbb2a73917
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-04-27 18:23:38 -0700

    Time: 226 ms (65.33%), Space: 15.4 MB (93.37%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -2,9 +2,9 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         nums.sort()
         for i in range(len(nums) - 1, 1, -1):
-        # if the sum of 2 sides is larger than the last side...
+        # if the sum of 2 sides is larger than the last side
             if(nums[i-2] + nums[i-1] > nums[i]):
-                # return the area of the triangle...
+                # return the area of the triangle
                 return nums[i] + nums[i-1] + nums[i-2]
         return 0
             
