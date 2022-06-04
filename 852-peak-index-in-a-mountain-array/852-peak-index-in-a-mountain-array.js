51a90e4764660b530af3c2839916d121ec698330
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-03 21:56:22 -0700

    Time: 107 ms (12.44%), Space: 42.5 MB (71.94%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i+1] < arr[i]) {
+            return i
+        } else {
+            continue
+        }
+    }
+};
