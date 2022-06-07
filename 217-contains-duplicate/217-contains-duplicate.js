8db76fa55497c2a7324c290da7285bc653dbc41e
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-06 20:02:07 -0700

    Time: 104 ms (70.74%), Space: 51.5 MB (28.55%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,14 +2,24 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+//     let map = {};
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] in map) {
+//             return true;
+//         } else {
+//             map[nums[i]] = nums[i];
+//         }
+//     }
+//     return false;
+// };
+
+// more efficient?
 var containsDuplicate = function(nums) {
-    let map = {};
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] in map) {
-            return true;
-        } else {
-            map[nums[i]] = nums[i];
-        }
+    const set = [...new Set(nums)];
+    if (set.length === nums.length) {
+        return false;
+    } else {
+        return true;
     }
-    return false;
 };
