25297816909ba01ac80d361fba88f59b6f88df91
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-12 15:11:49 -0700

    Time: 281 ms (63.09%), Space: 21 MB (80.17%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,9 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        answer = [];
+        # creating a fixed array is more efficient because the array does not need to dynamically change size?
+        answer = [1] * (len(nums));
         prefix = 1;
         for i in range(len(nums)):
-            answer.append(prefix)
+            answer[i] = prefix
             prefix *= nums[i]
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
