a5fae1c98f4c73442d6ab7b0d168c23b78084809
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-12 15:03:59 -0700

    Time: 158 ms (37.64%), Space: 55.8 MB (30.61%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,29 +3,45 @@
  * @return {number[]}
  */
 
-// with division and extra pointers, also O(2n)
+// with division and extra pointers, also O(2n) = time: > 63.04%, space: > 42.98%
+// var productExceptSelf = function(nums) {
+//     let result = [];
+//     let product = 1;
+//     let productNoZero = 1;
+//     let zeroCount = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== 0) {
+//             productNoZero = productNoZero * nums[i];
+//         } else {
+//             zeroCount += 1;
+//         }
+//         product = product * nums[i];
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] === 0 && zeroCount < 2) {
+//             result.push(productNoZero);
+//         } else if (nums[i] === 0 && zeroCount >= 2){
+//             result.push(0);
+//         } else {
+//             result.push(product / nums[i]);
+//         }
+//     }
+//     return result;
+// };
+
+// Without Division - calculate prefix values on first loop, then multiply those values by the postfix values on second loop
+// Time: O(n), Space: O(1) (output array doesn't count)
 var productExceptSelf = function(nums) {
-    let result = [];
-    let product = 1;
-    let productNoZero = 1;
-    let zeroCount = 0;
+    let answer = [];
+    let prefix = 1;
+    let postfix = 1;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            productNoZero = productNoZero * nums[i];
-        } else {
-            zeroCount += 1;
-        }
-        product = product * nums[i];
+        answer.push(prefix);
+        prefix *= nums[i];
     }
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0 && zeroCount < 2) {
-            result.push(productNoZero);
-        } else if (nums[i] === 0 && zeroCount >= 2){
-            result.push(0);
-        } else {
-            result.push(product / nums[i]);
-        }
-        
+    for (let i = nums.length - 1; i >= 0; i--) {
+        answer[i] *= postfix;
+        postfix *= nums[i]
     }
-    return result;
+    return answer;
 };
