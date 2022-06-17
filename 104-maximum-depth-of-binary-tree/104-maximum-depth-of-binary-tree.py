b209e7c8f7c9a9c6effae2544900414184902885
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-17 13:46:06 -0700

    Time: 59 ms (52.75%), Space: 15.2 MB (98.61%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -5,10 +5,34 @@
 #         self.left = left
 #         self.right = right
 
-# First Solution = recursive, O(n)
+# First Solution = recursive DFS, O(n)
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1;
+    
+# Second Solution = iterative BFS, O(n)
+# deque = "double ended queue" - doubly linked list that gives O(1) pop and push for head and tail
+from collections import deque
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1;
+        level = 0
+        queue = deque([root])
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+            
+        return level
+        
+        
