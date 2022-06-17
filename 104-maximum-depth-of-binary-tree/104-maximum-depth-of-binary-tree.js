75e4535bd3546fbfbe0bd894e3fff53bfcba97d7
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-17 14:46:45 -0700

    Time: 83 ms (71.22%), Space: 45 MB (65.06%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// Recursive DFS O(n), 
+var maxDepth = function(root) {
+    if (!root) return null;
+    
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
