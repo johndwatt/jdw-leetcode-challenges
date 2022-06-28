b9d3a54a4118f9df663149d2b80b8dbefeac3e63
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-06-27 22:56:31 -0700

    Time: 93 ms (48.75%), Space: 44.2 MB (53.29%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let [prev, current] = [null, head];
+    while (current) {
+        temp = current.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+    return prev;
+};
