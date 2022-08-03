490fac2f1e5352a9b45f184fd10c6c535118bb54
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-08-02 18:00:23 -0700

    Time: 279 ms (5.00%), Space: 50.2 MB (12.95%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    // Remove all non alphabetical chars and make lower case
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // Compare back half of the string to front half
+    let j = str.length - 1;
+    for (let i = 0; i < str.length / 2; i++) {
+        console.log(str[i], str[j])
+        if (str[i] !== str[j]) return false;
+        j--;
+    }
+    return true;
+};
