d465bc5183d59d50873505bde7788dbe2cde2c00
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-09-09 15:45:08 -0700

    Time: 54 ms (99.46%), Space: 15.1 MB (49.64%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,19 @@
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     map = {}
+    #     for i in range(len(nums)):
+    #         map[nums[i]] = i
+    #     for i in range(len(nums)):
+    #         comp = target - nums[i]
+    #         if comp in map and map[comp] != i:
+    #             return [i, map[comp]]
+    
+    
+    # one pass solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}
         for i in range(len(nums)):
-            map[nums[i]] = i
-        for i in range(len(nums)):
-            comp = target - nums[i]
-            if comp in map and map[comp] != i:
-                return [i, map[comp]]
+            if target - nums[i] in map:
+                return [map[target - nums[i]], i]
+            else:
+                map[nums[i]] = i
