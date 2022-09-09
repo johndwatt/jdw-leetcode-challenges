1b6647b5831c9ad8eb81aedc8819af0f8a37310c
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-09-09 15:39:29 -0700

    Time: 154 ms (24.31%), Space: 42.8 MB (86.76%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -26,26 +26,51 @@
 // };
 
 // cleaner, more readable approach, and more efficient
+// var isAnagram = function(s, t) {
+//     if (s.length !== t.length) return false;
+//     const obj1 = {};
+//     const obj2 = {};
+//     for (let i = 0; i < s.length; i++){
+//         if (s[i] in obj1 === true){
+//             obj1[s[i]] += 1;
+//         } else {
+//             obj1[s[i]] = 1;
+//         }
+//         if (t[i] in obj2 === true){
+//             obj2[t[i]] += 1;
+//         } else {
+//             obj2[t[i]] = 1;
+//         }
+//     }
+//     for (let letter in obj1) {
+//         if (obj1[letter] === obj2[letter]) {
+//             continue;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+// EVEN MORE readable approach
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
     const obj1 = {};
     const obj2 = {};
     for (let i = 0; i < s.length; i++){
-        if (s[i] in obj1 === true){
+        if (s[i] in obj1){
             obj1[s[i]] += 1;
         } else {
             obj1[s[i]] = 1;
         }
-        if (t[i] in obj2 === true){
+        if (t[i] in obj2){
             obj2[t[i]] += 1;
         } else {
             obj2[t[i]] = 1;
         }
     }
     for (let letter in obj1) {
-        if (obj1[letter] === obj2[letter]) {
-            continue;
-        } else {
+        if (obj1[letter] !== obj2[letter]) {
             return false;
         }
     }
