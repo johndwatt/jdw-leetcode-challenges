c792a59e327ffdef3378c35de03366c8dfd0a4f4
John D. Watt <86326516+johndwatt@users.noreply.github.com>
2022-09-12 14:23:50 -0700

    Time: 123 ms (42.63%), Space: 43.4 MB (75.36%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -2,13 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+// not linear runtime
+// var singleNumber = function(nums) {
+//     nums.sort((a,b) => a - b);
+//     for (let i = 0; i < nums.length; i++){
+//         if (nums[i] !== nums[i+1]) {
+//             return nums[i];
+//         } else {
+//             i++;
+//         }
+//     }
+// };
+
 var singleNumber = function(nums) {
-    nums.sort((a,b) => a - b);
+    let result = 0;
     for (let i = 0; i < nums.length; i++){
-        if (nums[i] !== nums[i+1]) {
-            return nums[i];
-        } else {
-            i++;
-        }
+        result = result ^ nums[i];
     }
+    return result;
 };
